fb6ee3903e26f1e7657e802754114ebaafc3b467
LineBreez <alinesn0308@gmail.com>
2022-01-27 01:11:15 -0300

    desafio6

---
+++ script.js
@@ -0,0 +1,76 @@
+// Crie um objeto que receba ao menos três propriedades sobre você.
+
+let sobremim = {
+    nome:"Aline",
+    idade:26,
+    estilomusical:[ "Hip hop", "Charme","Trap"]
+   
+}
+console.log(sobremim)
+
+// Adicione uma nova propriedade sem alterar seu objeto inicial.
+
+sobremim.corDeCabelo = "Loiro"
+console.log(sobremim)
+
+// Remova uma propriedade desse objeto.
+
+delete sobremim.corDeCabelo
+console.log(sobremim)
+
+//Mostre no console todas as propriedades desse objeto.
+
+console.log(sobremim)
+
+
+// Crie um array  chamado "cadastro" contendo ao menos 5 objetos. 
+
+cadrasto
+
+state = {
+    devsVnw: [
+      {
+        name: "aline",
+        age: 15,
+        telefone: "123456789"
+      },
+      {
+        name: "Leona",
+        age: 16,
+        telefone: "123456789"
+      },
+      {
+        name: "cida",
+        age: 17,
+        telefone: "123456789"
+      }
+      {
+        name: "paulo",
+        age: 18,
+        telefone: "123456789"
+      }
+      {
+        name: "tita",
+        age: 19,
+        telefone: "123456789"
+      }
+    ]
+  }   
+  render(){
+    return(
+      <div>
+        <p>{this.state.devsVnw[1].name}</p>
+      </div>
+
+
+//Cada objeto deve receber as seguintes propriedades: nome,  idade,  telefone, amigos. 
+//Na propriedade amigos, adicione ao menos 4 itens.
+
+let idade =  [15,16,17,18,19]
+let pessoas = ["tita","cida","paulo","leona","aline"]
+console.log(pessoas [2])
+
+
+// Mostre no console o nome de um amigo de cada lista.
+
+console.log(cida [2])
